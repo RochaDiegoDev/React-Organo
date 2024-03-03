31611b52307a063bd5b61c5664a46ab8a279cf51
DIEGO ROCHA <eng.diegor@gmail.com>
2024-03-02 22:10:26 -0400

    Deletando o Colaborador

--- src/componentes/Colaborador/index.js
+++ src/componentes/Colaborador/index.js
@@ -5,7 +5,12 @@ import './colaborador.css'
 
 const Colaborador = ({ colaborador, corDeFundo, aoDeletar,  }) => {
     return (<div className="colaborador">
-        <IoIosCloseCircleOutline size={30} color="white" className='deletar' onClick={aoDeletar}/>
+        <IoIosCloseCircleOutline 
+        size={30} 
+        color="white" 
+        className='deletar' 
+        onClick={() => aoDeletar(colaborador.id)}
+        />
 
         <div className="cabecalho" style={{ backgroundColor: corDeFundo }}>
             <img src={colaborador.imagem} alt={colaborador.nome} />
