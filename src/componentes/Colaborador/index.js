9018ccf83198ed1516747acd38fc5df67c4838dd
DIEGO ROCHA <eng.diegor@gmail.com>
2024-03-06 21:20:33 -0400

    Finalizando o Curso

--- src/componentes/Colaborador/index.js
+++ src/componentes/Colaborador/index.js
@@ -1,9 +1,22 @@
-import { IoIosCloseCircleOutline } from "react-icons/io";
+import { IoIosCloseCircleOutline} from "react-icons/io";
+import { IoIosHeartEmpty } from "react-icons/io";
+import { IoIosHeart } from "react-icons/io";
 
 
 import './colaborador.css'
 
-const Colaborador = ({ colaborador, corDeFundo, aoDeletar,  }) => {
+const Colaborador = ({ colaborador, corDeFundo, aoDeletar,aoFavoritar }) => {
+
+    function favoritar(){
+        aoFavoritar(colaborador.id);
+    }
+
+    const propsFavorito={
+        size: 25,
+        onClick: favoritar
+    }
+
+
     return (<div className="colaborador">
         <IoIosCloseCircleOutline 
         size={30} 
@@ -14,12 +27,18 @@ const Colaborador = ({ colaborador, corDeFundo, aoDeletar,  }) => {
 
         <div className="cabecalho" style={{ backgroundColor: corDeFundo }}>
             <img src={colaborador.imagem} alt={colaborador.nome} />
-        </div>
+        </div>z
 
         <div className="rodape">
             <h4>{colaborador.nome}</h4>
             <h5>{colaborador.cargo}</h5>
-        </div>        
+                <div className="favoritar">
+                    {colaborador.favorito ? 
+                    <IoIosHeart size={25} onClick={favoritar} color="red"/> 
+                    : <IoIosHeartEmpty size={25} onClick={favoritar}
+                />}
+                </div>
+        </div>
     </div>)
 }
 
